a2fd08549f63a05bb3fe2b2a15a8b45835a0e335
Dragos-Catalin Pop <cpop@pitechplus.com>
2016-10-10 15:00:31 +0300

    6th chapter

--- src/app.php
+++ src/app.php
@@ -11,23 +11,29 @@ function is_leap_year($year = null) {
   return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year %100);
 }
 
-$routes = new Routing\RouteCollection();
-$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
-  'year' => null,
-  '_controller' => function ($request) {
-    if (is_leap_year($request->attributes->get('year'))) {
+class LeapYearController
+{
+  public function indexAction($year)
+  {
+    if (is_leap_year($year)) {
       return new Response('Yep, this is a leap year!');
     }
 
     return new Response('Nope, this is not a leap year.');
   }
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+  'year' => null,
+  '_controller' => 'LeapYearController::indexAction',
 )));
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
+/*$routes->add('hello', new Routing\Route('/hello/{name}', array(
   'name' => 'World',
   '_controller' => 'render_template',
 )));
 $routes->add('bye', new Routing\Route('/bye', array(
   '_controller' => 'render_template',
-)));
+)));*/
 
 return $routes;
